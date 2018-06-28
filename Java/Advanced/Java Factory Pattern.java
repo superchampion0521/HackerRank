05b6575e330e97a5ad7809a39fe75ec75f5e2d0a
SuperChampion <superchampion0521@gmail.com>
2018-06-28 01:17:22 -0400

    Solution for "Java Factory Pattern" in Java/Advanced

---
+++ Java/Advanced/Java Factory Pattern.java
@@ -0,0 +1,10 @@
+            //Write your code here
+            switch (order.toLowerCase()) {
+                case "pizza": 
+                    return new Pizza();
+                case "cake":
+                    return new Cake();
+                default: 
+                    return null;
+            }
+            
