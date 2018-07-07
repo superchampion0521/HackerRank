a0a4fc77b29a5a07d907dbc96349726dec305a76
SuperChampion <superchampion0521@gmail.com>
2018-07-07 11:00:23 -0400

    Solution for "Java BigDecimal" in Java/BigNumber

---
+++ Java/BigNumber/Java BigDecimal.java
@@ -0,0 +1,12 @@
+        //Write your code here
+        Comparator<String> customComparator = new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                BigDecimal a = new BigDecimal(s1);
+                BigDecimal b = new BigDecimal(s2);
+                return b.compareTo(a); // descending order
+            }
+        };
+
+        Arrays.sort(s, 0, n, customComparator);
+        
