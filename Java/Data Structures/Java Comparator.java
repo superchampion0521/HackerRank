4a19a2a899742d078dc0fa115ed318e7c098fc71
SuperChampion <superchampion0521@gmail.com>
2018-07-15 01:25:34 -0400

    Solution for "Java Comparator" in Java/Data Structures

---
+++ Java/Data Structures/Java Comparator.java
@@ -0,0 +1,13 @@
+// Write your Checker class here
+import java.util.*;
+
+class Checker implements Comparator<Player> {
+    @Override
+    public int compare(Player p1, Player p2) {
+        if (p1.score == p2.score) {
+            return p1.name.compareTo(p2.name);
+        } else {
+            return p2.score - p1.score; // descending order
+        }
+    }
+}
