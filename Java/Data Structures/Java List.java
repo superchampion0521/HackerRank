8cb8d7f45dd8b81f16ee600959b9e6b4f4902959
SuperChampion <superchampion0521@gmail.com>
2018-07-20 00:42:29 -0400

    Solution for "Java List" in Java/Data Structures

---
+++ Java/Data Structures/Java List.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        /* Create and fill Linked List of Integers */
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            int value = scan.nextInt();
+            list.add(value);
+        }
+        
+        /* Perform queries on Linked List */
+        int Q = scan.nextInt();
+        for (int i = 0; i < Q; i++) {
+            String action = scan.next();
+            if (action.equals("Insert")) {
+                int index = scan.nextInt();
+                int value = scan.nextInt();
+                list.add(index, value);
+            } else { // "Delete"
+                int index = scan.nextInt();
+                list.remove(index);
+            }
+        }
+        scan.close();
+        
+        /* Print our updated Linked List */
+        for (Integer num : list) {
+            System.out.print(num + " ");
+        }
+    }
+}
