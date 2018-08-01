598ab823b1a4a2f7b93b3066930478998416aa86
SuperChampion <superchampion0521@gmail.com>
2018-08-01 00:27:04 -0400

    Solution for "Java Lambda Expressions" in Java/Introduction

---
+++ Java/Introduction/Java Lambda Expressions.java
@@ -0,0 +1,29 @@
+   // Write your code here
+   public static PerformOperation isOdd() {
+    return n -> (n & 1) == 1;
+}
+
+public static PerformOperation isPrime() {
+    // O(n^(1/2)) runtime
+    return n -> {
+        if (n < 2) {
+            return false;
+        }
+        int sqrt = (int) Math.sqrt(n);
+        for (int i = 2; i <= sqrt; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    };
+}
+
+public static PerformOperation isPalindrome() {
+    return n -> {
+        String original = Integer.toString(n);
+        String reversed = new StringBuilder(Integer.toString(n)).reverse().toString();
+        return original.equals(reversed);
+    };
+}
+}
