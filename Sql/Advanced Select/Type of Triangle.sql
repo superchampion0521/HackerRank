6d9ba09b5fd7fb43d311fb6524d3080a31f29775
SuperChampion <superchampion0521@gmail.com>
2018-09-17 03:01:36 -0400

    Solution for "Type of Triangle" in Sql/Advanced Select

---
+++ Sql/Advanced Select/Type of Triangle.sql
@@ -0,0 +1,15 @@
+/*
+Enter your query here.
+*/
+SELECT
+  CASE
+    WHEN A = B AND B = C then "Equilateral"
+    WHEN A + B <= C then "Not A Triangle"
+    WHEN A + C <= B then "Not A Triangle"
+    WHEN B + C <= A then "Not A Triangle"
+    WHEN A = B AND A <> C then "Isosceles"
+    WHEN A = C AND A <> B then "Isosceles"
+    WHEN B = C AND A <> B then "Isosceles"
+    ELSE "Scalene"
+  END
+FROM triangles
