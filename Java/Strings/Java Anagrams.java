21315a54c8b8bc4cccfef1707a9ae43de0842a4b
SuperChampion <superchampion0521@gmail.com>
2018-09-25 01:13:35 -0400

    Solution for "Java Anagrams" in Java/Strings

--- Java/Strings/Java Anagrams.java
+++ Java/Strings/Java Anagrams.java
@@ -1,63 +1,15 @@
 static boolean isAnagram(String a, String b) {
-    // Complete the function
-    boolean anagram = true;
-  Character[] charSet =   {'A','B','C','D','E',
-                        'F','G','H','I','J',
-                        'K','L','M','N','O',
-                        'P','Q','R','S','T',
-                        'U','V','W','X','Y','Z'};
-  HashMap<Character,Integer> ALetterFrequency = new HashMap<Character,Integer>();
-  HashMap<Character,Integer> BLetterFrequency = new HashMap<Character,Integer>();
-  intializeHash(ALetterFrequency, charSet);       //O(n)
-  intializeHash(BLetterFrequency, charSet);       //O(n)
-  a = a.toUpperCase(); //O(n)
-  b = b.toUpperCase(); //O(n)
-  
-  //Originally I took a different approach to this problem because I was using substring and it 
-  // runs in O(n) which would make my algorithm run in O(n^2) with constant space, but then I
-  // remembered that charAt() runs in constant time so utilizing that function I was able to
-  // have a time complexity of O(n) and space complexity of O(1)
-  
-  for(int i = 0;i <a.length();i++)  //O(n)
-  {
-      Character letter = a.charAt(i);   //O(1)
-      Integer frequency = ALetterFrequency.get(letter);
-      ALetterFrequency.put(letter,++frequency);
-  }
-  for(int i = 0;i <b.length();i++)  //O(n)
-  {
-      Character letter = b.charAt(i);   //O(1)
-      Integer frequency = BLetterFrequency.get(letter);
-      BLetterFrequency.put(letter,++frequency);
-  }
-  
-  
-  //Compare Hash
-  for(Character letter : charSet)
-  {
-      if(!ALetterFrequency.get(letter).equals(BLetterFrequency.get(letter)))
-      {
-          anagram = false;
-      }
-  }
-  
-  return anagram;
-}
-
-static void intializeHash(HashMap hash, Character[] set)
-{
-  for(Character letter : set)
- {
-      hash.put(letter,0);
- }
-      
-}
-static Character[] toCharacterArray(char[] cArray)
-{
-  Character[] CArray = new Character[cArray.length];
-  for(int i =0; i<cArray.length;i++)
-  {
-      CArray[i] = (Character) cArray[i];
-  }
-  return CArray;
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+    
+    char[] c = a.toCharArray();
+    java.util.Arrays.sort(c);
+    char[] d = b.toCharArray();
+    java.util.Arrays.sort(d);
+    String A = new String(c);
+    String B = new String(d);
+    if (A.equals(B)) {
+        return true;
+    }
+    return false;
 }
