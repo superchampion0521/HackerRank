61642b541e891c46f74311bc7c15c020c45f31eb
SuperChampion <superchampion0521@gmail.com>
2018-10-06 15:07:55 -0400

    Solution for "Weather Observation Station 20" in Sql/Aggregation

---
+++ Sql/Aggregation/Weather Observation Station 20.sql
@@ -0,0 +1,6 @@
+/*
+Enter your query here.
+*/
+SELECT round(S.LAT_N,4)
+FROM station AS S
+WHERE (SELECT count(Lat_N) FROM station WHERE Lat_N < S.LAT_N ) = (SELECT count(Lat_N) FROM station WHERE Lat_N > S.LAT_N);
